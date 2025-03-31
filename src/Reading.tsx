e9aed213a496a50652094e8a372acc48d10502e8
lukamircetic <luka.mircetic@gmail.com>
2025-03-30 22:07:12 -0400

    changed where the pagination goes based on vp height

--- src/Reading.tsx
+++ src/Reading.tsx
@@ -8,16 +8,37 @@ function Reading() {
     useReading();
   const [currentPage, setCurrentPage] = useState(1);
   const [openArticle, setOpenArticle] = useState(-1);
+  const [listHeight, setListHeight] = useState("875");
+
   if (isLoading) return <div>Loading articles... </div>;
   if (error) return <div>Error loading articles, please try again later </div>;
   // console.log(articles);
   useEffect(() => {
     fetchArticlesByPage(currentPage);
+    const height = getListHeight();
+    setListHeight(height);
   }, [currentPage]);
+
+  const getListHeight = () => {
+    const viewPortHeight = window.innerHeight;
+    const lastPage = Math.ceil(totalArticles / 10);
+    const numArticlesOnLastPage = totalArticles % 10;
+    if (
+      currentPage !== lastPage ||
+      numArticlesOnLastPage === 0 ||
+      viewPortHeight >= 1200
+    ) {
+      return "875";
+    }
+    return `${150 + 80 * (numArticlesOnLastPage - 1)}`;
+  };
 
   return (
     <div className="flex flex-col gap-4 max-w-2xl">
-      <div className="flex flex-col gap-6 min-h-[875px]">
+      <div
+        className={`flex flex-col gap-6`}
+        style={{ minHeight: `${listHeight}px` }}
+      >
         {articles &&
           articles.map((article) => (
             <Article
